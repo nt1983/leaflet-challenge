19c0ea8251beec06fa5d4d5d5aeb446b0e1523a1
nt1983 <73156205+nt1983@users.noreply.github.com>
2021-03-31 14:08:13 -0700

    latest code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,41 +1,68 @@
+var earthquake_json="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// var API_KEY="pk.eyJ1IjoibnQxOTgzIiwiYSI6ImNrbWh6ZjRlMzBjOHUyb3RuZWNoaWk3YmgifQ.ZgFls354gv7BM5FiIZje_Q"
+function markersize(magnitude) {
+    if (magnitude ==0) {return 1; }
+    else {return magnitude*4;}
+}
+
 // Create the satellite layer that will be the background of our map
-var Satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var initLayer= L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 15,
-    id: "mapbox.satellite",
+    maxZoom: 10,
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
 });
 
-// Create the GrayScale layer that will be the background of our map
-var GrayScale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 15,
-    id: "mapbox.dark",
-    accessToken: API_KEY
+// Create the map object with options
+var mymap = L.map("mapid", {
+    center: [51.505, -0.09],
+    zoom: 5
 });
 
-// Create the GrayScale layer that will be the background of our map
-var Outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 15,
-    id: "mapbox.outdoors",
-    accessToken: API_KEY
-});
-  
-    // Create a baseMaps object to hold the map layers
-    var baseMaps = {
-      "Satellitep": Satellite,
-      "GrayScale": GrayScale,
-      "Outdoors":Outdoors
-    };
-  
-// Initialize all of the LayerGroups we'll be using
-var layers = {
-    COMING_SOON: new L.LayerGroup(),
-    EMPTY: new L.LayerGroup(),
-    LOW: new L.LayerGroup(),
-    NORMAL: new L.LayerGroup(),
-    OUT_OF_ORDER: new L.LayerGroup()
-  };
-  
- 
+initLayer.addTo(mymap);
+    
+function chagecolor (magnitude) {
+    switch (magnitude) {
+      case magnitude > 5:
+        return "red";
+      case magnitude > 4:
+        return "redorange";
+      case magnitude > 3:
+        return "orange";
+      case magnitude > 2:
+        return "yellow";
+      case magnitude > 1:
+         return "green";
+    }
+} 
+//create style of circle marker
+function style(feature) 
+{
+  return {
+    opacity: 1,
+    fillOpacity: 0.75,
+    fillColor: chagecolor(feature.properties.mag),
+    color: "#000000",
+    radius: markersize(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  }
+};
+
+//Creating layer groups for control chekcboxes
+var quakeLayer = new L.LayerGroup() ;
+var plateLayer = new L.LayerGroup() ;
+
+d3.json(earthquake_json).then(function (data) {
+    console.log(data);
+    L.geoJson(data, {
+        pointToLayer: function(feature,latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        onEachFeature: function(feature, layer) {
+
+        }
+    })
+
+    }).addTo(quakeLayer);
